af65346ad4333a424365353f84312ad121bb2679
Valter Valik <valtervaralik@gmail.com>
2023-02-22 20:10:40 -0500

    adding helmet security

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const passportLocal = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -45,11 +46,13 @@ app.use(express.json());
 app.use(method_override('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 const sessionConfig = {
+	name: 'rftx',
 	secret: 'thisshouldbeabettersecret',
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
+		// secure:true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	},
@@ -57,7 +60,60 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+//security
 app.use(mongoSanitize());
+app.use(helmet());
+
+const scriptSrcUrls = [
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net/',
+];
+
+const styleSrcUrls = [
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net/',
+];
+
+const connectSrcUrls = [
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://b.tiles.mapbox.com/',
+	'https://events.mapbox.com/',
+];
+
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", 'blob:'],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/dsknqsk7x/',
+				'https://images.unsplash.com/',
+			],
+			fontSrc: ["'self'", 'data:', ...fontSrcUrls],
+		},
+	})
+);
+app.use(helmet.crossOriginEmbedderPolicy({ policy: 'credentialless' }));
+app.use(helmet.crossOriginOpenerPolicy({ policy: 'unsafe-none' }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 app.use(passport.initialize());
 app.use(passport.session());
